9e986b0ce6d51a5c637b21f84c1b16a71091ffda
fioreColoca <fiorecoloca99@gmail.com>
2019-09-15 18:16:32 -0300

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -6,37 +6,38 @@ import java.util.Scanner;
     Integer opcion;
     Integer resultado;
       do{
+    	  Scanner teclado = newScanner (System.in);
       System.out.println("Ingrese su operación /n 1 para restar /n 2 para sumar /n 3 para dividir /n 4 para multiplicar y /n 0 para salir");
         opcion = teclado.next();
-      }while(opcion < 1 && opcion > 4)
+      }while(opcion < 1 && opcion > 4);
       switch (opcion) {
         case 1:
         System.out.println("Ingrese el primer numero");
-          a = teclado.next();
+          a = teclado.nextInt();
         System.out.println("Ingrese el segundo numero");
-          a = teclado.next();
+          a = teclado.nextInt();
           resultado = a - b;
         break;
 
         case 2:
         System.out.println("Ingrese el primer numero");
-          a = teclado.next();
+          a = teclado.nextInt();
         System.out.println("Ingrese el segundo numero");
           a = teclado.next();
           resultado = a + b;
         break;
         case 3:
         System.out.println("Ingrese el primer numero");
-          a = teclado.next();
+          a = teclado.nextInt();
         System.out.println("Ingrese el segundo numero");
-          a = teclado.next();
+          a = teclado.nextInt();
         resultado = a / b;
         break;
         case 4:
         System.out.println("Ingrese el primer numero");
-          a = teclado.next();
+          a = teclado.nextInt();
         System.out.println("Ingrese el segundo numero");
-          a = teclado.next();
+          a = teclado.nextInt();
         resultado = a * b;
         break;
       }
